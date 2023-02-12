135f58435ad13c8900012edea4a70afd8089ed46
Monil Patel <105329178+MonilPatel03@users.noreply.github.com>
2023-02-13 02:19:01 +0530

    commit 2
    Pom updated
    request mapping added

---
+++ plantplaces/src/main/java/com/plantplaces/plantplaces/PlantPlacesController.java
@@ -0,0 +1,25 @@
+package com.plantplaces.plantplaces;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+
+public class PlantPlacesController {
+	
+	@RequestMapping(value="/start", method=RequestMethod.GET)
+	public String read(){
+		return "start";
+	}
+	@PostMapping("/start")
+	public String create() {
+		return "start";
+	}
+	
+	@RequestMapping("/")
+	public String index() {
+		return "start";
+	}
+}
